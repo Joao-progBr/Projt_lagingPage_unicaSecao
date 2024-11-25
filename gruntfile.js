444d0f14cdc949b756029ee3fe544f91bbdfc145
joãoA <joaovictor.dudu03@gmail.com>
2024-11-25 07:21:55 -0300

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,108 @@
+const { option } = require("grunt");
+
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // env: {
+        //     dev: {
+        //       NODE_ENV: 'development',
+        //     },
+        //     prod: {
+        //       NODE_ENV: 'production',
+        //     },
+        //   },
+      
+          // Configuração do SASS
+        sass: {
+            dev: {
+              options: {
+                style: 'expanded', // Mais legível, útil para desenvolvimento
+                },
+              files: {
+                'dev/styles/main.css': 'src/styles/main.scss', 
+                },
+            },
+            prod: {
+              options: {
+                style: 'compressed', 
+                sourcemap: false,    
+              },
+              files: {
+                'dist/styles/main.min.css': 'src/styles/main.scss', 
+              },
+            },
+        },
+        watch:{
+            sass: {
+                files: ['src/styles/**/main.scss'],
+                tasks : ['sass:dev']
+            },
+            html:{
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_CSS',
+                            replacement: './styles/main.css',
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_CSS',
+                            replacement: './styles/main.min.css',
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'prebuild/index.html' : 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild']
+    })
+
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+
+    grunt.registerTask('default', ['watch']),
+    grunt.registerTask('build', ['sass:prod', 'htmlmin:dist', 'replace:dist', 'clean' ])
+}
